cc88444534652a471f0287344d1bfe05e31e480a
ArnoldM <atmadisa1@gmail.com>
2022-09-22 09:40:41 +0200

    [Arnold] initial commit

---
+++ src/test/java/com/assessment/ClientManagementSystem/ClientManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.ClientManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
